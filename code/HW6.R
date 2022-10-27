d34f82d0096c7668744e2acde740e574b297f3f3
Ian Hart <ian.hart167@gmail.com>
2022-10-27 14:41:48 -0500

    for loop tingz

--- code/HW6.R
+++ code/HW6.R
@@ -10,27 +10,43 @@ yp = 1
 df<- data.frame(matrix(ncol=4, nrow=101))
 colnames(df)<- c('day', 'zoop','macinv', 'biomass')
 
-df$day[1] = 1
+df$day = seq(0,100)
 df$zoop[1] = zoop
 df$macinv[1] = macinv
 df$biomass = 0
 
 # outer for loop
+
 current_pop = c(rep(colnames(df[2]), times = df[1,2]),
                 rep(colnames(df[3]), times = df[1,3]))
 
-biomass = 0
+for (i in 1: nrow(df)){
+  bm = 0
+  
+  # inner for loop
+  
+  for (j in 1:50){
+    
+    food = sample(current_pop, size = 1)
+    
+    current_pop = current_pop[-match(food, current_pop)]
+    
+    bm = bm + ifelse(food == 'macinv', 10, 1)
+    
+    if  (bm >= 50 ) break
+    
+  }
+  
+  # update current population to data frame
+  
+  df$biomass[i] = bm
+  
+  df$zoop[i] = length(current_pop[current_pop == 'zoop'])
+  df$macinv[i] = length(current_pop[current_pop == 'macinv'])
+  
+  
+  
 
-# inner for loop
-food = sample(current_pop, size = 1)
-current_pop = ifelse(food == 'zoop', current_pop[current_pop == 'zoop'][- (1:1)],
-                     current_pop[current_pop == 'macinv'][- (1:1)] )
+}
 
-bm = ifelse(food == 'macinv', 10, 1)
-
-df$biomass[1] = df$biomass[1] + bm
-
-if  (df$biomass[1] => 50 ) break
-
-# update current population to data frame
 
